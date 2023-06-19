cea6492cc1c4ffc6f8aea34f4abd2e520e950214
Anton Petrov <a.petrov.83@gmail.com>
2023-06-19 19:55:28 +0300

    added solutions

---
+++ 01_JS_Syntax_Fundamentals/Lab/03_excellent_grade.js
@@ -0,0 +1,9 @@
+function solve(number) {
+    if (number >= 5.50) {
+        console.log('Excellent')
+    }else{
+        console.log('Not excellent')
+    }
+}
+
+solve(5.49);
---
+++ 01_JS_Syntax_Fundamentals/Lab/04_month_printer.js
@@ -0,0 +1,24 @@
+function solve(number) {
+    const monthMap = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'
+    }
+
+    if (number >= 1 && number <= 12) {
+        console.log(monthMap[number])
+    } else {
+        console.log('Error!')
+    }
+}
+
+solve(5)
