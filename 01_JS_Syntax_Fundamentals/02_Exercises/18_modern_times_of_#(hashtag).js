25ea3932f8000d2359699158f4fee6dfd9ab539b
Anton Petrov <a.petrov.83@gmail.com>
2023-06-22 20:04:07 +0300

    made changes on solution

--- 01_JS_Syntax_Fundamentals/02_Exercises/18_modern_times_of_#(hashtag).js
+++ 01_JS_Syntax_Fundamentals/02_Exercises/18_modern_times_of_#(hashtag).js
@@ -2,19 +2,18 @@ function modernTimes(inputString) {
     let words = inputString.split(' '); // string to array of words
 
     for (const word of words) {
-        if (word.startsWith('#') && word.length > 1) { // checks if word starts with '#' and its length is bigger than 1
-            if (validWord(word.substring(1))) { 
-                console.log(word.substring(1));
-            }
-
+        // checks if word starts with '#' and its length is bigger than 1 and is word is valid(only letters)
+        if (word.startsWith('#') && word.length > 1 && validWord(word.substring(1))) { 
+            console.log(word.substring(1));
         }
 
     }
 
     function validWord(checkedWord) {
         let isValid = true;
-        
-        for (const ch of checkedWord.toLowerCase()) {
+        let wordToLowerCase = checkedWord.toLowerCase();
+
+        for (const ch of wordToLowerCase) {
             if (!(ch.codePointAt(0) >= 97 && ch.codePointAt(0) <= 122)) {
                 isValid = false;
                 break;
