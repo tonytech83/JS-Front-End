7a46e0da5151fc72974b7a68cdd7095b235f00d7
Anton Petrov <a.petrov.83@gmail.com>
2023-06-26 17:02:11 +0300

    add documentation

--- 02_Functions_and_Statements/01_Lab/06_sign_check.js
+++ 02_Functions_and_Statements/01_Lab/06_sign_check.js
@@ -1,7 +1,12 @@
+/**
+ * Checks whether the result of the multiplication of received numbers is positive or negative.
+ * @param  numbers {Array} numbers Three positive or negative number
+ * @returns {String} 'Positive' or 'Negative'
+ */
 function signCheck(...numbers) {
   return numbers
     .filter(number => number < 0) // filter only negative numbers
-    .length % 2 === 0 ? 'Positive' : 'Negative' // check if length divided by two is zero or one 
+    .length % 2 === 0 ? 'Positive' : 'Negative' // check if length of new array divided by two is zero or one 
 
 };
 
