39a0c4af23e351064a45311579ac24aa443e9431
Anton Petrov <a.petrov.83@gmail.com>
2023-07-13 15:41:41 +0300

    added solution

--- 04_DOM_and_Events/03_More_Exercises/08_hells_kitchen/app.js
+++ 04_DOM_and_Events/03_More_Exercises/08_hells_kitchen/app.js
@@ -1,8 +1,58 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  function onClick() {
+    const data = JSON.parse(document.getElementById('inputs').getElementsByTagName('textarea')[0].value);
+    const bestRestaurant = document.querySelector('#bestRestaurant > p');
+    const workers = document.querySelector('#workers > p');
+    let restaurants = {};
+
+    for (const restaurantData of data) {
+      let [name, workersData] = restaurantData.split(' - ');
+
+      if (!restaurants.hasOwnProperty(name)) {
+        restaurants[name] = {
+          workers: {},
+          bestSalary: 0,
+          totalSalary: 0,
+          avgSalary: 0,
+        };
+      }
+
+      let workersInfo = workersData.split(', ');
+      for (const worker of workersInfo) {
+        let [workerName, workerSalary] = worker.split(' ');
+
+        restaurants[name].workers[workerName] = Number(workerSalary);
+
+        if (restaurants[name].bestSalary < Number(workerSalary)) {
+          restaurants[name].bestSalary = Number(workerSalary);
+        }
+
+        restaurants[name].totalSalary += Number(workerSalary);
+
+      }
+    }
+
+    for (const name in restaurants) {
+      let numberOfWorkers = Object.entries(restaurants[name].workers).length
+      restaurants[name].avgSalary = restaurants[name].totalSalary / numberOfWorkers
+    }
+
+    let sortByAvgSalary = Object.entries(restaurants)
+      .sort((a, b) => b[1].avgSalary - a[1].avgSalary)
+
+    let best = sortByAvgSalary[0]
+
+    bestRestaurant.textContent = `Name: ${best[0]} Average Salary: ${best[1].avgSalary.toFixed(2)} Best Salary: ${best[1].bestSalary.toFixed(2)}`
+    let bestWorkers = Object.entries(best[1].workers)
+      .sort((a, b) => b[1] - a[1])
+
+    for (const [workerName, workerSalary] of bestWorkers) {
+      workers.textContent += `Name: ${workerName} With Salary: ${workerSalary} `
+    }
+
+  }
+
 }
+
